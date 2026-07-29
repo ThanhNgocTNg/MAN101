d9b257e9ee8becc1bda53912387338430e36d3b1
Thành Ngọc Lê <thanhngoc.contact@gmail.com>
2026-07-29 08:20:41 +0700

    Add files via upload

---
+++ assets/js/tailwind.config.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        "rikkei-red": "#C00000",
+        "rikkei-darkred": "#900000",
+        "rikkei-navy": "#3C2F6A",
+        "rikkei-lightnavy": "#F3F2F8"
+      }
+    }
+  }
+}
